271c07fd4911bea853f90d77cf7cce44b2fd09b5
ojeelafriend <63030605+ojeelafriend@users.noreply.github.com>
2025-03-07 19:16:42 -0300

    [fix] delete basePath

--- edoze-ui/next.config.ts
+++ edoze-ui/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/edoze",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "cdn.dummyjson.com", pathname: "**" },

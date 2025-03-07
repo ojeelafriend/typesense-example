9bfe47639fff937a2847d6bb0f3a8fa2336891d7
ojeelafriend <63030605+ojeelafriend@users.noreply.github.com>
2025-03-07 18:46:33 -0300

    [modify] basePath for production

--- edoze-ui/next.config.ts
+++ edoze-ui/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/edoze",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "cdn.dummyjson.com", pathname: "**" },
